489a9276808ac130f3ea314ebd89954f3b2f3ade
Dennis Vo <dennisphuc@gmail.com>
2025-03-11 11:18:06 +0100

    feat: day 3, in 30 days UIChallenges, design a simple modal

---
+++ day3/script.js
@@ -0,0 +1,19 @@
+var btnOpen = document.querySelector(".open-modal-btn");
+var modal = document.querySelector(".modal");
+var iconClose = document.querySelector(".modal-header i");
+var btnClose = document.querySelector(".modal-footer button");
+
+function toggleModal() {
+  modal.classList.toggle("hide");
+}
+
+btnOpen.addEventListener("click", toggleModal);
+
+iconClose.addEventListener("click", toggleModal);
+btnClose.addEventListener("click", toggleModal);
+
+modal.addEventListener("click", function (e) {
+  if (e.target == e.currentTarget) {
+    toggleModal();
+  }
+});
