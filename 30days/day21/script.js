6adc654470242879626554d9c74f1478a745f3be
Dennis Vo <dennisphuc@gmail.com>
2025-05-23 10:57:06 +0200

    feat: update day 21 with full dark, light mode

--- 30days/day21/script.js
+++ 30days/day21/script.js
@@ -1,11 +1,23 @@
 const input = document.querySelector("#switch");
+const switchWrapper = document.querySelector(".switch-wrapper");
 const body = document.querySelector("body");
 
+function init() {
+  let mode = localStorage.getItem("mode");
+  body.setAttribute("class", mode);
+}
+
+init();
+
 input.addEventListener("click", function (e) {
-  let value = this.checked;
-  if (value) {
-    body.classList.add("dark");
-  } else {
-    body.classList.remove("dark");
-  }
+  body.classList.toggle("dark");
+  let mode = body.getAttribute("class") ? "dark" : "";
+  localStorage.setItem("mode", mode);
+});
+
+switchWrapper.addEventListener("click", function (e) {
+  input.checked = !input.checked;
+  body.classList.toggle("dark");
+  let mode = body.getAttribute("class") ? "dark" : "";
+  localStorage.setItem("mode", mode);
 });
